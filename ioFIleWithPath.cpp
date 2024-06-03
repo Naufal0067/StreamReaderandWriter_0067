3fcb4c2f9a4e4d86df63596a7a21e0057305f689
Naufal0067 <naufalopang1928@gmail.com>
2024-06-03 14:56:02 +0700

    melakukan perulangan setiap baris

--- ioFIleWithPath.cpp
+++ ioFIleWithPath.cpp
@@ -39,4 +39,11 @@ int main() {
     cout << endl << ">= Membuka dan membaca fle " << endl;
     //jika file ada maka
     if  (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            
+        }
+    }
 }
