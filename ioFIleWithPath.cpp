45ba7e33b2f9404d360a8e484641ff5b99b3ec96
Naufal0067 <naufalopang1928@gmail.com>
2024-06-03 14:55:18 +0700

    jika file ada maka if infile.is_open

--- ioFIleWithPath.cpp
+++ ioFIleWithPath.cpp
@@ -35,4 +35,8 @@ int main() {
 
     //menunjuk ke sebuah file
     infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca fle " << endl;
+    //jika file ada maka
+    if  (infile.is_open())
 }
